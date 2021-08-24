35c1df96b17d992b17e62bfe0b51acbd246349a2
Ashtar Paniagua <74083145+AshtarCodes@users.noreply.github.com>
2021-08-24 17:04:58 -0400

    test husky

--- packages/server/testing-linting.js
+++ packages/server/testing-linting.js
@@ -5,5 +5,5 @@ var didLintingHookWork = true;
                 let working = didLintingHookWork; let myobj = { hello: true, yoo: "true", husky: "sucks", team: "istheshit" }; module.exports = { working, myobj };
 
                 
-
+var newthing;
 
