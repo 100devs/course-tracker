8cd8f28c61e8b2506834ef2766d0160c0f0c2143
dvkr <v3dvkr@gmail.com>
2021-09-02 22:15:41 -0600

    filled in auth routes
    new PUT request to change password
    
    added dev routes
    
    passwords are hashed

--- packages/server/middleware/auth.js
+++ packages/server/middleware/auth.js
@@ -1,5 +1,8 @@
 module.exports = {
   checkAdmin: function (req, res, next) {
+    if (process.env.NODE_ENV !== "production") {
+      next();
+    }
     if (req.session.isAdmin) {
       next();
     } else {
--- packages/server/models/User.js
+++ packages/server/models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema(
   {
@@ -22,5 +23,23 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function () {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  } catch (err) {
+    throw new Error(err);
+  }
+});
+
+// TODO: use this to clean up authRoutes.js post login
+// userSchema.methods.comparePassword = function(candidatePassword) {
+//   bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+//       if (err) return console.log(err);
+//       console.log(isMatch);
+//       return isMatch;
+//   });
+// };
 
 module.exports = model("User", userSchema);
