d5355952fd6a2f67d967794c1f76de8e78acf3cc
Arielle Morabito <74011011+relspeedwagon@users.noreply.github.com>
2021-09-10 14:16:36 -0400

    hopefully resolves yarn.lock conflict, deletes unused var in tandem with ticket 27

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -2,8 +2,7 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
-  return <div className="App">
-  </div>;
+  return <div className="App"></div>;
 }
 
 export default App;
