e631c917d5911c1097b1b6e448ef1b27b7aa2a67
Arielle Morabito <74011011+relspeedwagon@users.noreply.github.com>
2021-09-10 14:11:35 -0400

    hopefully resolves yarn.lock conflict, deletes unused var  in tandem with ticket 27

--- packages/server/models/Post.js
+++ packages/server/models/Post.js
@@ -1,30 +1,32 @@
 const mongoose = require("mongoose");
 
-const PostSchema = new mongoose.Schema({
+const PostSchema = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     body: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     isDraft: {
-        type: Boolean,
-        required: true,
+      type: Boolean,
+      required: true,
     },
-    publishedAt:{
-        type: Date
-    }
-}, {timestamps: true})
+    publishedAt: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
 
-PostSchema.pre('save', function save(next){
+PostSchema.pre("save", function save() {
+  const changes = this.getChanges();
 
-    const changes = this.getChanges()
-    
-    if(changes.isDraft === false){
-        this.publishedAt = new Date()
-    }
-})
+  if (changes.isDraft === false) {
+    this.publishedAt = new Date();
+  }
+});
 
 module.exports = mongoose.model("Post", PostSchema);
