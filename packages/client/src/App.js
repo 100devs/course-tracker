781350d31fa25e0f19599e127708a59b5999335a
Madeline Hassett <hassettmadeline@gmail.com>
2021-09-13 15:49:37 -0400

    remove unused response variables from authRoutes in server

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,23 +1,21 @@
-import './App.css';
-import React from 'react';
+import "./App.css";
+import React from "react";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from 'react-router-dom';
-import Post from './components/Post';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Post from "./components/Post";
 
 //eslint-disable-next-line import/no-webpack-loader-syntax
-import Content from '!babel-loader!@mdx-js/loader!./markdown/Content.mdx'
+import Content from "!babel-loader!@mdx-js/loader!./markdown/Content.mdx";
 
 function App() {
   return (
     <div className="App">
-        <Post>
-          <Content />
-        </Post>
+      <Post>
+        <Content />
+      </Post>
+      <Post>
+        <Content />
+      </Post>
     </div>
   );
 }
