9671a621f7edc1d7ad51fa2daf86c286a090922e
Mario De Los Santos <mario.jdls@gmail.com>
2021-09-20 22:19:36 -0400

    added endpoints to get all posts, get all published posts and get a post by id with admin check

---
+++ packages/server/controllers/get.js
@@ -0,0 +1,22 @@
+const Post = require("../models/Post");
+
+module.exports = {
+    allPosts: async (req, res) => {
+        const posts = await Post.find().sort({createdAt: 'desc'}).lean()
+        res.json({posts})
+    },
+    published: async (req, res) => {
+        const posts = await Post.find({isDraft: false}).sort({publishedAt: 'desc'}).lean()
+        res.json({posts})
+    },
+    postById: async (req, res) => {
+        const isAdmin = req.session.isAdmin
+        const {id} = req.params.id
+        const post = await Post.findById({_id: id})
+        if(post.isDraft && !isAdmin){
+            res.status(401).json({message: 'Permission denied'})
+        }else{
+            res.json({post})
+        }
+    }
+}
---
+++ packages/server/routes/getRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const getController = require("../controllers/get");
+const { checkAdmin } = require("../middleware/auth");
+
+router.get("/all-posts", checkAdmin, getController.allPosts);
+router.get("/published", getController.published)
+router.get("/post/:id", getController.postById)
+
+module.exports = router;
