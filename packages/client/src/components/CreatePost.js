a03898e1537b9eb37a564a7414f070673ad96042
Madeline Hassett <hassettmadeline@gmail.com>
2021-09-24 14:25:17 -0400

    moved styled components to styled directory

--- packages/client/src/components/CreatePost.js
+++ packages/client/src/components/CreatePost.js
@@ -1,13 +1,13 @@
 import { useState } from "react";
-import ButtonDiv from "./ButtonDiv";
-import Button from "./Button";
-import InputDiv from "./InputDiv";
-import Input from "./Input";
-import InputLabel from "./InputLabel";
-import TextArea from "./TextArea";
-import Checkbox from "./Checkbox";
-import Form from "./Form";
-import FormHeader from "./FormHeader";
+import ButtonDiv from "./styled/ButtonDiv";
+import Button from "./styled/Button";
+import InputDiv from "./styled/InputDiv";
+import Input from "./styled/Input";
+import InputLabel from "./styled/InputLabel";
+import TextArea from "./styled/TextArea";
+import Checkbox from "./styled/Checkbox";
+import Form from "./styled/Form";
+import FormHeader from "./styled/FormHeader";
 
 // Data looks like this (for backend)
 // console.log({
