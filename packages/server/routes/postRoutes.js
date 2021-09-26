86ea5dd4b2b06a988553cd4cad83ea6dbe3a08c4
Yolanda Haynes <yhaynes@giyo.us>
2021-09-26 15:18:31 -0700

    updated files from latest main branch

--- packages/server/routes/postRoutes.js
+++ packages/server/routes/postRoutes.js
@@ -3,12 +3,7 @@ const router = express.Router();
 const postsController = require("../controllers/posts");
 const { checkAdmin } = require("../middleware/auth");
 
-<<<<<<< HEAD
 router.post("/createPost", checkAdmin, postsController.createPost);
 router.put("/editPost/:id", checkAdmin, postsController.editPost);
-=======
-router.post("/create-post", postsController.createPost);
-router.put("/edit-post/:id", postsController.editPost);
->>>>>>> dcab7fe (updated client and server paths to shish-kebab-case)
 
 module.exports = router;
