45f2e6386b1d54147afaf7a00046b29edc343676
dvkr <v3dvkr@gmail.com>
2021-10-20 15:17:03 -0600

    added functionality to process token and verify user is admin

--- packages/server/middleware/auth.js
+++ packages/server/middleware/auth.js
@@ -3,24 +3,26 @@ const User = require("../models/User");
 // if (req.session.isAdmin) <= prev session check
 
 const checkAdmin = async (req, res, next) => {
-  const token = req.headers.authentication;
-
+  const token =
+    req.headers.authentication ||
+    req.query.authentication ||
+    req.headers["x-access-token"];
   if (!token) {
     return res.status(403).send({ message: "Token is not valid!" });
   }
 
   try {
-    const decoded = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const accessToken = JSON.parse(token)["accesstoken"];
+    const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
     req.user = await User.findOne({ _id: decoded.userId });
-    req.adminStatus = req.user.isAdmin;
-    //return next()
     if (req.user.isAdmin) {
       return next();
     } else {
-      return res.redirect("/");
+      return res.status(400).send({ message: "User is not an admin" });
     }
   } catch (err) {
+    console.log(err);
     return res.status(403).send({ message: "Access forbidden!" });
   }
 };
-module.exports = checkAdmin;
+module.exports = { checkAdmin };
