9662109b51de3f4ac6e24cd5613f078452737b28
Madeline Hassett <hassettmadeline@gmail.com>
2021-11-02 20:55:05 -0400

    adding form validation for login

--- packages/client/src/components/Login.js
+++ packages/client/src/components/Login.js
@@ -10,18 +10,30 @@ import FormHeader from "./styled/FormHeader";
 import Container from "./styled/Container";
 import TextLink from "./TextLink";
 import { Redirect } from "react-router-dom";
-import { loginValidation } from "../formValidation";
+import { emailValidation, passwordValidation } from "../formValidation";
 
 //need to figure out how to work this validation into form, maybe create a useForm useEffect, change/submit handlers, handle errors for backend
+//add touched logic rendering?
+const validate = {
+  email: emailValidation,
+  password: passwordValidation,
+};
 
 const Login = () => {
+  //cancel state
   const [cancel, setCancel] = useState(false);
+  //set errors state
+
+  //importing user object and login function from AuthContext
   const { user, login } = useContext(AuthContext);
+
+  //creating state for user object set to empty email/pw values
   const [userObj, setUserObj] = useState({
     email: "",
     password: "",
   });
 
+  //handle change
   function updateUser(e) {
     const { name, value } = e.target;
     setUserObj({
@@ -29,19 +41,24 @@ const Login = () => {
       [name]: value,
     });
   }
+  //handle blur and validate
 
+  //handle submit - add validation logic
   const loginFunc = async (e) => {
     e.preventDefault();
+    //should this be await? if the logic check out, call login, otherwise, display frontend errors
     login(userObj);
+    //handle backend errors
   };
 
+  //handle cancel
   if (cancel) {
     return <Redirect to="/" />;
   }
 
   return (
     <Container minHeight="100vh">
-      <AuthForm height="auto">
+      <AuthForm validate={validate} height="auto">
         <FormHeader>
           <h2>Task Lemon</h2>
         </FormHeader>
--- packages/client/src/formValidation.js
+++ packages/client/src/formValidation.js
@@ -1,17 +1,36 @@
 module.exports = {
-  loginValidation: function (values) {
-    let errors = {};
-    // email
-    if (!values.email.trim()) {
-      errors.email = "Email is required";
-    } else if (!/\S+@\S+\.\S+/.test(values.email)) {
-      errors.email = "Email address is invalid";
+  emailValidation: function (email) {
+    if (
+      /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+        email
+      )
+    ) {
+      return null;
     }
-
-    // password
-    if (!values.password) {
-      errors.password = "Password is required";
+    if (email.trim() === "") {
+      return "Email is required";
     }
-    return errors;
+    return "Please enter a valid email";
   },
+
+  passwordValidation: function (password) {
+    if (password.trim() === "") {
+      return "Password is required";
+    }
+  },
+  // loginValidation: function (values) {
+  //   let errors = {};
+  //   // email
+  //   if (!values.email.trim()) {
+  //     errors.email = "Email is required";
+  //   } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+  //     errors.email = "Email address is invalid";
+  //   }
+
+  //   // password
+  //   if (!values.password) {
+  //     errors.password = "Password is required";
+  //   }
+  //   return errors;
+  // },
 };
