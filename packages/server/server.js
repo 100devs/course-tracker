b065b0c28eef837abdee240ecaa2d234fb63b93e
Thomas Dillard <thomas.dillard.studios@gmail.com>
2021-11-21 13:24:11 -0700

    fixing more errors

--- packages/server/server.js
+++ packages/server/server.js
@@ -38,7 +38,7 @@ app.use("/api", apiRoutes);
 
 // Code that tells heroku where to find react build files
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
