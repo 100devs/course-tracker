9e15aa132a62eba582077318774d939483a1e19b
Arielle Morabito <74011011+relspeedwagon@users.noreply.github.com>
2021-12-03 20:52:15 -0500

    deletes unnec comment missed in prev commit

--- packages/client/src/components/Logo.js
+++ packages/client/src/components/Logo.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { ReactComponent as TextLogo } from "../assets/task-lemon-txt-logo.svg";
-// import { ReactComponent as TextLogo } from "../assets/text-logo.svg";
 import { ReactComponent as LemonLogo } from "../assets/lemon-logo.svg";
 
 const Logo = ({ lemonView }) => {
