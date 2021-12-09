8e1306beb55d2de99f6a46e72145b78b1a037137
Arielle Morabito <74011011+relspeedwagon@users.noreply.github.com>
2021-12-09 10:47:50 -0500

    adds comment to Logo.js re: AModuleScopePlugin conflict should be revisited after MVP

--- packages/client/src/components/Logo.js
+++ packages/client/src/components/Logo.js
@@ -6,3 +6,8 @@ const Logo = ({ lemonView }) => {
   return <>{lemonView ? <LemonLogo /> : <TextLogo />}</>;
 };
 export default Logo;
+
+// ASSETS was moved to SRC to avoid import conflict for logos, but should ultimately be in PUBLIC
+// (create-react-app uses ModuleScopePlugin to ensure that relative imports from app's source directory don't reach outside of it...)
+
+// There are better work-arounds to address this, should be revisited after MVP  --relspeedwagon
